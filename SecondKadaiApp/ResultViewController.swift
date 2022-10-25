f4310c6aafdbe9a80d821e17756b2a7aba21b418
takahiro.koizumi <ktakahiro091@icloud.com>
2022-10-26 00:22:48 +0900

    二回目の課題です。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by koi on 2022/10/25.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var name: String = "名前"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let namae=name
+        label.text="こんにちわ、\(namae)さん"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
